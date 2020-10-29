488c59a4ad464b77300563242f5b61af1814da36
JOCOghub <jojocohen57@gmail.com>
2020-10-29 13:05:59 -0400

    removes commented out in indexjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const header = document.getElementById("header")
-  const form = document.getElementById('orchestraForm') 
+  const form = document.getElementById('orchestraForm') //might need to be listForm
   const mainContainer = document.getElementById('container')
 
   form.addEventListener('submit', Orchestra.createOrchestra)
@@ -7,7 +7,7 @@ const header = document.getElementById("header")
 
   function reset(){
     mainContainer.innerHTML = ''
-    mainContainer.innerHTML += `<form id="orchestraForm"> 
+    mainContainer.innerHTML += `<form id="orchestraForm">
       <label for="">Name:</label>
       <input type="text" id="orchestraName">
       <input type="submit" >
@@ -26,49 +26,3 @@ const header = document.getElementById("header")
 
 
   Orchestra.fetchOrchestras()
-
-
-
-
-
-
-// function createList() {
-//   event.preventDefault()
-//   const name = document.getElementById('listName').value
-//   document.getElementById('listName').value = ""
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify({list: {name: name}})
-//   }
-//
-//   fetch("http://localhost:3000/lists", options)
-//   .then(r => r.json())
-//   .then(listObj => renderList(listObj.data) )
-// }
-
-
-// function renderList(list) {
-//   let div = document.getElementById('listContainer')
-//   let pgh = document.createElement('p')
-//   pgh.innerText = list.attributes.name
-//   div.append(pgh)
-// }
-//
-// function renderLists(lists) {
-//   for (list of lists) {
-//     renderList(list)
-//   }
-// }
-//
-//
-// function fetchLists() {
-//   fetch("http://localhost:3000/lists")
-//   .then(r => r.json())
-//   .then(lists => renderLists(lists.data))
-// }
-
-// fetchLists()
