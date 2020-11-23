03ad296ce2a86b84f3672ccefb8889f9e8c07be3
JOCOghub <jojocohen57@gmail.com>
2020-11-23 15:54:45 -0500

    back in black baybe

--- src/index.js
+++ src/index.js
@@ -14,22 +14,15 @@ function reset(){
     <input type="text" id="orchestraName">
     <input type="submit" >
   </form>`
-  mainContainer.innerHTML +=`<form id="findForm">
-  <label for="">Find Instrument</label>
-  <input type="text" id="queryInput" >
-  <input type="submit"> 
-</form>`
   addListeners()
 }
 
 function addListeners() {
   const form = document.getElementById('orchestraForm')
   const div = document.createElement('div')
-  const form2 = document.getElementById('findForm')
   div.id = "orchestraContainer"
   mainContainer.append(div)
   form.addEventListener('submit', Orchestra.createOrchestra)
-  form2.addEventListener('submit', Instrument.findInstrument) 
   Orchestra.renderOrchestras()
 }
 
